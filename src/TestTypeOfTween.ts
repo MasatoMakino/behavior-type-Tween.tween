ff2857b743080faab695936f7ece2ccba53f302c
MasatoMakino <unagiinu.g@gmail.com>
2020-10-24 12:53:01 +0900

    update : devDependencies to "@tweenjs/tween.js": "18.6.4"

--- src/TestTypeOfTween.ts
+++ src/TestTypeOfTween.ts
@@ -1,9 +1,4 @@
-import TWEEN from "@tweenjs/tween.js";
 import {Tween} from "@tweenjs/tween.js";
 
-const tweenArray01 : TWEEN.Tween[] = []; //NG
-const tweenArray02 : Tween[] = [];  //NG
-
-const tween01 = new TWEEN.Tween({x:1.0}); //OK
-const tween02:TWEEN.Tween = new TWEEN.Tween({x:1.0}); //NG
-const tween03:Tween = new TWEEN.Tween({x:1.0}); //NG
+const tweenArray : Tween<Record<string, unknown>>[] = [];  //OK
+const tween:Tween<{ x:number }> = new Tween({x:1.0}); //OK
